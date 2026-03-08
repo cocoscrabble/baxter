a2504ef4039f68614b483162fbd947cf97070592
Martin DeMello <martindemello@gmail.com>
2026-03-07 18:50:44 -0800

    fix static url

--- baxter/settings.py
+++ baxter/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
